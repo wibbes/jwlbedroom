1d2488a2d8e7d95b6502c23a52b23927d74673ce
james lyons <lobster94@outlook.com>
2018-10-20 17:29:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+'use strict';
+
 //Bedroom
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
-const url = require('url');
-
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
 
-const server = express()
-  .use((req, res) =>{ res.sendFile(INDEX) })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.json());
+app.post('/forecast', function(req, res) {
+    // We'll fill this out later!
+    res.json({ hello: 'world' });
+});
+app.listen(PORT);
